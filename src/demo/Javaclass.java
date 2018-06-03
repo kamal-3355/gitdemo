52dc774938ca3eb1c6bf6ef5144afac4f97d16a5
kamal-3355 <39656672+kamal-3355@users.noreply.github.com>
2018-06-02 23:10:45 -0700

    restructuring  the project contents

---
+++ src/demo/Javaclass.java
@@ -0,0 +1,52 @@
+package demo;
+
+public class Javaclass {
+	public int id;
+	public String name;
+	public float address;
+	
+
+	public Javaclass(int id, String name, float address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public float getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(float address) {
+		this.address = address;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/demo/codeimp.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class codeimp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Javaclass java = new Javaclass(11,"kamal", 231);
+System.out.println(java.getId());
+System.out.println(java.getAddress());	
+System.out.println(java.getName());
+
+}}
